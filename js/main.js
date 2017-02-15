9cef72d018f68b548b876a6327485ade9e26605e
Bence Kiss <kiss.bence.balint@gmail.com>
2017-02-15 16:10:25 -0500

    Added javascript/jquery so the switch changes the background and the text on the bottom.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-
-
+$(function(){
+  var lightText = "Now it is light";
+  var darkText = "Now it is dark";
+  $('body').addClass('light');
+  $('.switch button').on('click', function(){
+    $(this).toggleClass('off on');
+    $('body').toggleClass('light dark')
+    $('.status').text($('body').attr('class') === 'light' ? lightText : darkText)
+  });
+  $('.status').html(lightText)
+});
